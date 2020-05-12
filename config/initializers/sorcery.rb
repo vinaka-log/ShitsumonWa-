8634d0ec53638180c1be9ef641f251f0419f983e
github yuta kawaoka <k64.yuta29@gmail.com>
2020-05-12 19:35:20 +0900

    add submodules-user_activation

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,13 +4,17 @@
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
 # :magic_login, :external
-Rails.application.config.sorcery.submodules = [:remember_me]
+Rails.application.config.sorcery.submodules = [:remember_me, :user_activation]
 
 # Here you can configure each submodule's features.
+Rails.application.config.sorcery.submodules = [:remember_me, :user_activation]
+
 Rails.application.config.sorcery.configure do |config|
   config.user_config do |user|
     user.remember_me_for = 1209600
-  end
+    user.user_activation_mailer = UserMailer
+  end 
+
   
   config.user_class = "User"
   # -- core --
