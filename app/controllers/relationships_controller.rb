91b59433ea0df64423d0e968b13acfe0bc4fbc41
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-02 00:07:39 +0900

    edit relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,31 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
+  def create
+    following = current_user.follow(@uer)
+    if following.save
+       flash[:success] = 'follow success!!'
+       redirect_to @user
+    else 
+      flash.now[:alert] = 'follow failed!!'
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:success] = 'unfollow success!!'
+      redirect_to @user
+    else
+      flash.now[:alert] = 'unfollow failed'
+      redirect_to @user
+    end
+  end
+
+    private
+    def
+      @user = User.find(params[:relationship][:follow_id])
+    end
+
 end
