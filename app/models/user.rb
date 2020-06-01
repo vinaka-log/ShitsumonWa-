959a977653f7dee6b723d554005cb9e0d97cea52
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-01 23:48:00 +0900

    add user model association

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,10 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_questions, through: :likes, source: 'question'
   has_many :comments
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+  
   
 end
