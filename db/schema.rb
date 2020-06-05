3b559fde48129096b863f787214e2feb20e6a4a3
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-05 17:38:48 +0900

    remake migration add column to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_074948) do
+ActiveRecord::Schema.define(version: 2020_06_05_083505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_074948) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "image"
+    t.integer "likes_count"
   end
 
   create_table "relationships", force: :cascade do |t|
