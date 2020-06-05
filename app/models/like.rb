69f958fc9fe20846feedcabd8075f91c57a4b13f
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-05 16:48:21 +0900

    edit like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
 
   validates :user_id, presence: true
   validates :question_id, presence: true
-  belongs_to :user
+  belongs_to :user, counter_cache: :likes_count
   belongs_to :question
   
 
