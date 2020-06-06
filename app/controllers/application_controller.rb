187a4ecb631cb8a92cafe32aa5f649658284845c
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-06 15:59:59 +0900

    add logged_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id])
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   private
   def not_authenticated
     redirect_to login_path, alert: "please login first"
