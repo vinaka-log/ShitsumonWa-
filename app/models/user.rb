fec7ff5dd710da0f11b770157b98f586625278f2
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-06 17:41:08 +0900

    fix signup&login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,12 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,format: { with: VALID_EMAIL_REGEX },
-              uniqueness: { case_sensitive: false }
-      
+              uniqueness: { case_sensitive: false }  
   validates :nationality, presence: true,length: { maximum: 45 }
-  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
   validates :password, confirmation: true, format: { with: VALID_PASSWORD_REGEX }
-
   validates :password_confirmation, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  validates reset_password_token, presence: true, uniqueness: true
+
 
   has_many :questions 
   has_many :likes, dependent: :destroy
