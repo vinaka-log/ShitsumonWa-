da52707c3a90c8aff933fa258465752474891015
TatsuroMoriyama <t4traw@gmail.com>
2020-06-10 21:27:27 +0900

    Remove unnecessary methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,10 @@
 class ApplicationController < ActionController::Base
-  
   protect_from_forgery with: :exception
-  
+
   add_flash_types :success, :info, :warning, :danger
 
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
-
   private
+
   def not_authenticated
     redirect_to login_path, alert: "please login first"
   end
