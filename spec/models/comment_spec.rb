460dd1431e0d0aa0fc0799ba885600a81df76893
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-15 00:17:07 +0900

    finish describe Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.create(:comment)
+  end
+
+  describe "comment" do
+    it "is valid with content" do
+      @comment.content = ""
+      expect(@comment).to be_invalid
+    end
+
+    it "is valid if content is  255 character" do
+      @comment.content = "a" * 255
+      expect(@comment).to be_valid
+    end
+
+    it "is valid if content is  256 character or more" do
+      @comment.content = "a" * 256
+      expect(@comment).to be_invalid
+    end
+  end
 end
