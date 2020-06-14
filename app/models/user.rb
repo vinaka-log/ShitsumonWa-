8ee8f5ba4ea451c987fb10a52280b343fa1bb7e4
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-14 18:47:42 +0900

    edit association comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :questions 
   has_many :likes, dependent: :destroy
   has_many :like_questions, through: :likes, source: 'question'
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,16 +17,101 @@ RSpec.describe User, type: :model do
       expect(@user).to be_invalid
     end
 
-    it "is valid if password is less 50 characters" do
+    it "is valid if name is less 50 characters" do
       @user.name = "a" * 50
       expect(@user).to be_valid
     end
 
-    it "is invalid if password is more 50 characters" do
+    it "is invalid if name is more 50 characters" do
       @user.name = "a" * 51
       expect(@user).to be_invalid
     end
   end
+
+  describe "email" do
+    it "is invalid without name" do
+      @user.email = ""
+      expect(@user).to be_invalid
+    end
+
+    it "is valid if email is 255 characters" do
+      @user.email = "a" * 243 + "@example.com"
+      expect(@user).to be_valid
+    end
+
+    it "is invalid if email is 256 characters" do
+      @user.email = "a" * 244 + "@example.com"
+      expect(@user).to be_invalid
+    end
+
+    it "is confirm valid email address" do
+      @user.email = "test@example.com"
+      expect(@user).to be_valid
+
+      @user.email = "TEST@example.COM"
+      expect(@user).to be_valid
+
+      @user.email = "T_Est@example.user.com"
+      expect(@user).to be_valid
+
+      @user.email = "test.user@example.jp"
+      expect(@user).to be_valid
+
+      @user.email = "user+test@example.net"
+      expect(@user).to be_valid
+    end
+
+    it "is confirm invalid email address" do
+      @user.email = "test@example,com"
+      expect(@user).to be_invalid
+
+      @user.email = "test_example.org"
+      expect(@user).to be_invalid
+
+      @user.email = "test.user@example."
+      expect(@user).to be_invalid
+
+      @user.email = "test@user_example.com"
+      expect(@user).to be_invalid
+
+      @user.email = "test@user+example.com"
+      expect(@user).to be_invalid
+    end
+     
+    it "is invalid if email is not uniqueness  " do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save!
+      expect(duplicate_user).to be_invalid
+    end
+
+    it "confirm email is saved in lowercase" do
+      @user.email = "Test@examplE.coM"
+      @user.save!
+      expect(@user.reload.email).to eq 'test@example.com'
+    end  
+  end
+
+  describe "nationality" do
+    it "is invalid without nationality" do
+      @user.nationality = ""
+      expect(@user).to be_invalid
+    end
+
+    it "is valid if nationality is less 45 characters" do
+      @user.nationality = "a" * 45
+      expect(@user).to be_valid
+    end
+
+    it "is invalid if nationality is more 45 characters" do
+      @user.nationality = "a" * 46
+      expect(@user).to be_invalid
+    end
+  end
+
+
+
+
      
 
 end
