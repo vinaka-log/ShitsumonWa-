73de4df6486e1a84388187f0c67a15fd65506237
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-14 22:38:06 +0900

    add image for test data

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :question do
     sequence(:name) { |n| "test_question#{n}" }
     sequence(:description) { |n| "test content" }
-    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test_question.jpeg')) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test_question.jpg')) }
     association :user
   end
 end
