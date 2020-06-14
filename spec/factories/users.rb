25ee600c228cd740f658fc8404fd246f74b6bc6b
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-14 16:58:21 +0900

    create factory :user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    factory :user do
+      sequence(:name) { |n| "test_user#{n}" }
+      sequence(:email) { |n| "test#{n}@example.com" }
+      nationality { "japan" }
+      password { "password" }
+      password_confirmation { "password" }
+    end 
   end
 end
