9b30e1d933ae449d617eb1fff53f726e6b25dce4
github yuta kawaoka <k64.yuta29@gmail.com>
2020-06-26 22:10:57 +0900

    add validation to stock.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,11 @@
 class Stock < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :user_id, presence: true
+  validates :question_id, presence: true
+
+  def self.get_stock_posts(user)
+    self.where(user_id: user.id).map(&:question)
+  end
 end
