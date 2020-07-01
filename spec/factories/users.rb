121b8681dba73ec0482d4a5a8e96a5f3d45f49a6
github yuta kawaoka <k64.yuta29@gmail.com>
2020-07-01 15:51:14 +0900

    pending

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,5 +9,19 @@ FactoryBot.define do
       after(:create) do |user|
         user.activate!
       end
-  end  
+  end 
+
+  factory :suzuki, class: User do
+      name { "suzuki" }
+      email { "suzuki@example.com" }
+      nationality { "japan" }
+      password { "suzuki1234" }
+      password_confirmation { "suzuki1234" }
+
+      after(:create) do |user|
+        user.activate!
+      end
+  end 
+  
+  
 end
--- spec/system/questions_spec.rb
+++ spec/system/questions_spec.rb
@@ -1,2 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Questions", type: :system do
+  
+before do
+    @question = FactoryBot.create(:question)
+end
+
+it "visiting questions/new" do
+  visit new_question_path
+  expect(page).to have_text("Let's ask about what you are interested in about Japan.")
+  expect(page).to have_text("New Question")
+  expect(page).to have_field("Title")
+  expect(page).to have_field("Content")
+  expect(page).to have_field("Image")
+  expect(page).to have_button("Submit")
+ end
 
 
+
+  
+end
