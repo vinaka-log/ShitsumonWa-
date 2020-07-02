84794bcf1a28c10b36c7a1f592ee4b46a035d968
github yuta kawaoka <k64.yuta29@gmail.com>
2020-07-02 15:19:38 +0900

    add signups teest

--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -1,54 +1,52 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Logins", type: :system do
+RSpec.describe 'Logins', type: :system do
+  let(:user) { create(:suzuki) }
 
-  let(:user){create(:suzuki)}
-  
   describe 'Visit' do
-
     # ログインページ
     it 'Login page' do
-        visit login_path
-        expect(page).to have_text('Log in')
-        expect(page).to have_text('Log in with Twitter')
-        expect(page).to have_text('Log in with Slack')
-        expect(page).to have_text('E-mail')
-        expect(page).to have_text('Password')
-        expect(page).to have_text('Log in')
-        expect(page).to have_text('Forgot Password?')
-        expect(page).to have_text('Email')
-        expect(page).to have_text('Click here if you have not registered')
-        expect(page).to have_text('Sign up')
+      visit login_path
+      expect(page).to have_text('Log in')
+      expect(page).to have_text('Log in with Twitter')
+      expect(page).to have_text('Log in with Slack')
+      expect(page).to have_text('E-mail')
+      expect(page).to have_text('Password')
+      expect(page).to have_text('Log in')
+      expect(page).to have_text('Forgot Password?')
+      expect(page).to have_text('Email')
+      expect(page).to have_text('Click here if you have not registered')
+      expect(page).to have_text('Sign up')
     end
   end
-  
-  describe 'Controll' do
-    context "Login", use_truncation: false do
-    
-    # ログイン成功
-    it 'is success that Creating new user with E-mail' do
-      visit login_path
-      fill_in 'E-mail', with: user.email, match: :first
-      fill_in 'Password', with: 'suzuki1234'
-      within '.login-form' do
-      click_button 'Log in'
-      end
-      expect(page).to have_content 'Login success'
-      expect(current_path).to eq user_path(user.id)
-    end
 
-    # ログイン失敗
-    it 'is fail that Creating new user with E-mail' do
-        create(:user)
+  describe 'Controll' do
+    context 'Login', use_truncation: false do
+      # ログイン成功
+      it 'is success that Creating new user with E-mail' do
         visit login_path
-        fill_in 'E-mail', with: 'test1@example.com',match: :first
+        fill_in 'E-mail', with: user.email, match: :first
+        fill_in 'Password', with: 'suzuki1234'
+        within '.login-form' do
+          click_button 'Log in'
+        end
+        expect(page).to have_content 'Login success'
+        expect(current_path).to eq user_path(user.id)
+      end
+
+      # ログイン失敗
+      it 'is fail that Creating new user with E-mail' do
+        visit login_path
+        fill_in 'E-mail', with: 'test1@example.com', match: :first
         fill_in 'Password', with: ''
         within '.login-form' do
-        click_on 'Log in'
+          click_on 'Log in'
         end
         expect(page).to have_content 'Login fail'
         expect(current_path).to eq login_pending_path
+      end
     end
-  end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :system do
 
+  let(:user){create(:suzuki)}
+
   # ユーザー登録ページ
-  describe 'Visit' do
+  describe 'Visit signup' do
     it 'Signup page' do
         visit signup_path
         expect(page).to have_text('Sign up')
@@ -16,7 +18,6 @@ RSpec.describe User, type: :system do
   end
 
   describe 'Controll' do
-
     # E-mailで新規ユーザーの作成に成功
     it 'is success that Creating new user with E-mail' do
       visit signup_registration_path
@@ -52,6 +53,51 @@ RSpec.describe User, type: :system do
           expect(page).to have_content 'Signup fail'
           expect(current_path).to eq signup_create_path
         end.to change(User, :count).by 0
+    end
+  end
+
+  describe 'Visit login' do
+    # ログインページ
+    it 'Login page' do
+      visit login_path
+      expect(page).to have_text('Log in')
+      expect(page).to have_text('Log in with Twitter')
+      expect(page).to have_text('Log in with Slack')
+      expect(page).to have_text('E-mail')
+      expect(page).to have_text('Password')
+      expect(page).to have_text('Log in')
+      expect(page).to have_text('Forgot Password?')
+      expect(page).to have_text('Email')
+      expect(page).to have_text('Click here if you have not registered')
+      expect(page).to have_text('Sign up')
+    end
+  end
+
+  describe 'Controll' do
+    context 'Login', use_truncation: false do
+      # ログイン成功
+      it 'is success that Creating new user with E-mail' do
+        visit login_path
+        fill_in 'E-mail', with: user.email, match: :first
+        fill_in 'Password', with: 'suzuki1234'
+        within '.login-form' do
+          click_button 'Log in'
+        end
+        expect(page).to have_content 'Login success'
+        expect(current_path).to eq user_path(user.id)
+      end
+
+      # ログイン失敗
+      it 'is fail that Creating new user with E-mail' do
+        visit login_path
+        fill_in 'E-mail', with: 'test1@example.com', match: :first
+        fill_in 'Password', with: ''
+        within '.login-form' do
+          click_on 'Log in'
+        end
+        expect(page).to have_content 'Login fail'
+        expect(current_path).to eq login_pending_path
+      end
     end
   end
 end
