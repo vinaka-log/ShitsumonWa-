159926cef25524e76417644071ca887a2ec63031
川岡優太 <kawaokayuta@vinaka.local>
2020-07-04 19:27:20 +0900

    visit new

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,28 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :system, js: true do
   let(:user_a){create(:suzuki)}
+  let(:user_b){create(:tanaka)}
   let!(:question_a) { FactoryBot.create(:question, name: 'first title', description: 'first content', user: user_a) }
 
   describe "visit new" do
-    # ログイン済みのユーザーならコメント可能
+    # ログイン済みのユーザーならコメント画面に遷移できる
     it "logged in user" do
       # ログインする
       visit login_path
-      fill_in 'E-mail', with: user_a.email, match: :first
-      fill_in 'Password', with: 'suzuki1234'
+      fill_in 'E-mail', with: user_b.email, match: :first
+      fill_in 'Password', with: 'tanaka1234'
       within '.login-form' do
       click_on 'Log in'
       end
       # 質問詳細画面
       visit question_path(id: question_a.id)
-      find('.delete-icon').click
-
+      find('.fa-comment').click
+      # 新規コメント画面
+      expect(page).to have_content 'New Comment!'
+      expect(page).to have_content 'Content'
+      expect(page).to have_button 'Comment'
     end
 
-    it "非ログインユーザー" do
-      visit post_path(@post.id)
-      expect(page).to have_selector '#comments'
-      expect(page).not_to have_selector '#comment_form'
+    # ログインしていないユーザーならコメント画面に遷移できない
+    it "not logged in user" do
+      # 質問詳細画面
+      visit question_path(id: question_a.id)
+      expect(page).not_to have_selector '.fa-comment'
     end
   end
 end
