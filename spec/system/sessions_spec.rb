6fd00ebf916676732a211db6a65e24c38f778f2d
川岡優太 <kawaokayuta@vinaka.local>
2020-07-05 01:59:09 +0900

    add logout test

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -22,30 +22,45 @@ RSpec.describe 'Logins', type: :system, js: true do
     end
   end
 
-  describe 'Controll' do
-    context 'Login', use_truncation: false do
+  describe 'lontroll' do
+    context 'login', use_truncation: false do
       # ログイン成功
-      it 'is success that Creating new user with E-mail' do
+      it 'is success that Creating new user' do
         visit login_path
         fill_in 'E-mail', with: user.email, match: :first
         fill_in 'Password', with: 'suzuki1234'
         within '.login-form' do
-          click_button 'Log in'
+          click_on 'Log in'
         end
         expect(page).to have_content 'Login success'
         expect(current_path).to eq user_path(user.id)
       end
 
       # ログイン失敗
-      it 'is fail that Creating new user with E-mail' do
+      it 'is fail that Creating new user with' do
         visit login_path
-        fill_in 'E-mail', with: 'test1@example.com', match: :first
+        fill_in 'E-mail', with: user.email, match: :first
         fill_in 'Password', with: ''
         within '.login-form' do
           click_on 'Log in'
         end
         expect(page).to have_content 'Login fail'
         expect(current_path).to eq login_pending_path
+      end
+    end
+
+    context 'logout', use_truncation: false do
+      # ログアウト
+      it 'is success that logout' do
+        visit login_path
+        fill_in 'E-mail', with: user.email, match: :first
+        fill_in 'Password', with: 'suzuki1234'
+        within '.login-form' do
+          click_on 'Log in'
+        end
+        click_on 'Log out'
+        expect(page).to have_content 'Logout!'
+        expect(current_path).to eq root_path
       end
     end
   end
