36689beca72844af9b109d7a49927de9a47c4ba7
川岡優太 <kawaokayuta@vinaka.local>
2020-07-05 22:26:32 +0900

    fin password reset mail

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,9 +2,15 @@ class PasswordResetsController < ApplicationController
   skip_before_action :require_login, raise: false
 
    def create
-     @user = User.find_by_email(params[:email])
-     @user.deliver_reset_password_instructions! if @user
-     redirect_to(root_path, success: 'Instructions have been sent to your email.')
+    @user = User.find_by_email(params[:email])
+
+    if @user
+      @user.deliver_reset_password_instructions! 
+      redirect_to(root_path, success: 'Instructions have been sent to your email')
+    else
+      flash.now[:danger] = 'Email address is wrong'
+      render template: "pages/index"
+    end
    end
 
    def edit
--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "PasswordResets", type: :system do
+
+  let(:user_a){create(:suzuki)}
   
   describe 'visit' do
     # パスワードリセットページ
@@ -8,10 +10,37 @@ RSpec.describe "PasswordResets", type: :system do
       visit login_path
       expect(page).to have_text('Forgot Password?')
       expect(page).to have_text('Email')
-      expect(page).to have_text('reset my password!')
+      expect(page).to have_button('Reset my password!')
     end
   end
-  
+
+  describe 'password reset mail' do
+    
+    # パスワード再設定手続きのメール送信に成功
+    it 'sends password reset mail (valid email)' do
+      visit login_path
+      within '.field' do
+        fill_in 'email', with: user_a.email
+      end
+        
+      click_on 'Reset my password!'
+      expect(current_path).to eq root_path 
+      expect(page).to have_text('Instructions have been sent to your email')
+    end
+
+    # パスワード再設定手続きのメール送信に成功（存在しないメールアドレス）
+    it "can't send password reset mail (invalid email)" do
+      visit login_path
+      
+      within '.field' do
+        fill_in 'email', with: 'tanaka@example.com'
+      end
+      click_on 'Reset my password!'
+      expect(current_path).to eq password_resets_path
+      expect(page).to have_text('Email address is wrong')
+    end
+  end
+
 
 end
 
