7f0fca256c483efce86d663226f36654efccef4d
川岡優太 <kawaokayuta@vinaka.local>
2020-07-20 23:46:57 +0900

    fix S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'shitsumonwa-production'
     config.fog_credentials = {
-      provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
-
-    config.fog_directory = 'shitsumonwa-production'
-    config.cache_storage = :fog
   end
 end 
 
