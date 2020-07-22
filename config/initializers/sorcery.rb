c85abb137e0bdee4969ae74777d8151c763da6cd
川岡優太 <kawaokayuta@vinaka.local>
2020-07-22 16:42:23 +0900

    fix sorcery.rb facebook

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -89,7 +89,7 @@ Rails.application.config.sorcery.configure do |config|
   # i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid, :salesforce, :slack, :line].
   # Default: `[]`
   #
-  config.external_providers = [:twitter, :slack]
+  config.external_providers = [:twitter, :slack, :facebook]
      
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -134,15 +134,15 @@ Rails.application.config.sorcery.configure do |config|
     name: 'name'
   }
   #
-  # config.facebook.key = ""
-  # config.facebook.secret = ""
-  # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
-  # config.facebook.user_info_path = "me?fields=email"
-  # config.facebook.user_info_mapping = {:email => "email"}
-  # config.facebook.access_permissions = ["email"]
-  # config.facebook.display = "page"
-  # config.facebook.api_version = "v2.3"
-  # config.facebook.parse = :json
+  config.facebook.key = Rails.application.credentials.dig(:facebook, :key)
+  config.facebook.secret = Rails.application.credentials.dig(:facebook, :secret_key)
+  config.facebook.callback_url = "https://www.shitsumonwa.com/oauth/callback?provider=facebook"
+  config.facebook.user_info_path = "me?fields=email"
+  config.facebook.user_info_mapping = { name: "name",email: "email"}
+  config.facebook.access_permissions = %w[email]
+  config.facebook.display = "page"
+  config.facebook.api_version = "v7.0"
+  config.facebook.parse = :json
   #
   # config.instagram.key = ""
   # config.instagram.secret = ""
