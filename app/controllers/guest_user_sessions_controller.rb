03f1b905fff451ad4611a96888a63147cd48abcf
川岡優太 <kawaokayuta@vinaka.local>
2020-07-25 15:46:32 +0900

    add guest user to top page

--- app/controllers/guest_user_sessions_controller.rb
+++ app/controllers/guest_user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class GuestUserSessionsController < ApplicationController
   def create
-    @user = User.find_by(email:"test@example.com")
+    @user = User.find_by(email:"yu-1luv.sr7gn@docomo.ne.jp")
     session[:user_id] = @user.id
     redirect_back_or_to(user_path(@user.id), success: 'Login success as guest user')
   end
