b57b98d7470c2abaef3d97bcb00e02a683391bf5
川岡優太 <kawaokayuta@vinaka.local>
2020-07-30 16:23:05 +0900

    create inquiry

--- app/controllers/guest_user_sessions_controller.rb
+++ app/controllers/guest_user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class GuestUserSessionsController < ApplicationController
   def create
-    @user = User.find_by(email:"test@example.com")
+    @user = User.find_by(email:"guest@example.com")
     session[:user_id] = @user.id
     redirect_back_or_to(user_path(@user.id), success: 'Login success as guest user')
   end
