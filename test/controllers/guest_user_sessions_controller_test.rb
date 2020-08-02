95aa562a46f681df047201a27ad30d881d8fb2a1
川岡優太 <kawaokayuta@vinaka.local>
2020-08-02 15:59:11 +0900

    fix user image source

--- test/controllers/guest_user_sessions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GuestUserSessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get guest_user_sessions_create_url
-    assert_response :success
-  end
-
-end
